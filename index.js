bd07e33ad8d395c8c136b8576c0906af11de2da5
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-09-26 07:02:18 +0600

    auto open

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const prompt = require('prompts')
 const shelljs = require('shelljs')
 const vbsPath = path.join(__dirname, './vbs.vbs')
 
+function runEmulator(name) {
+  console.log('Opening:', name)
+  shelljs.exec(`"${vbsPath}" ${name}`)
+}
+
 ;(async () => {
   const { stdout } = shelljs.exec('emulator -list-avds', {
     silent: true,
@@ -14,6 +19,15 @@ const vbsPath = path.join(__dirname, './vbs.vbs')
     .split('\n')
     .map((a) => a.trim())
 
+  if (emulators.length === 0) {
+    console.error('No emulator found!')
+    return process.exit(1)
+  }
+
+  if (emulators.length === 1) {
+    return runEmulator(emulators[0])
+  }
+
   const result = await prompt({
     name: 'name',
     type: 'select',
@@ -24,6 +38,5 @@ const vbsPath = path.join(__dirname, './vbs.vbs')
     })),
   })
 
-  console.log('Opening:', result.name)
-  shelljs.exec(`"${vbsPath}" ${result.name}`)
+  runEmulator(result.name)
 })()
